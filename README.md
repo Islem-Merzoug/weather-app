5ea8a811e0f4fe0b3626a38a193b0afeccf66624
Islem-Merzoug <i.merzoug16@gmail.com>
2021-11-20 19:41:56 +0100

    commit

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install
+expo start
+add WEATHER_API_KEY = ""; from https://home.openweathermap.org/api_keys
---
+++ utils/index.js
@@ -0,0 +1,5 @@
+export const colors = {
+    PRIMARY_COLOR: 'blue',
+    SECONDARY_COLOR: 'red',
+    BORDER_COLOR: 'green',
+}
